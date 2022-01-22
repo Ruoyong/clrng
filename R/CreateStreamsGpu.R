97df090ba847e7f02f89a95e86ffcea8ccf821e7
RY2268 <ruoyongxu2014@gmail.com>
2022-01-22 10:16:46 +0000

    colnames

--- R/CreateStreamsGpu.R
+++ R/CreateStreamsGpu.R
@@ -51,8 +51,7 @@
       if(!exists(".Random.seed.clrng")) {
         assign(".Random.seed.clrng", setBaseCreator())
       } 
-      
-
+                                   
       
       currentCreator = CreateStreamsBackend(
         .Random.seed.clrng,    
@@ -61,8 +60,12 @@
         onGpu=TRUE,
         keepInitial=TRUE)
       
-      assign(".Random.seed.clrng",  currentCreator, envir = .GlobalEnv)
+      # gpuR::colnames(streamsR) = c("current.g1.1", "current.g1.2", "current.g1.3", "current.g2.1", "current.g2.2", "current.g2.3",
+      #                              "initial.g1.1", "initial.g1.2", "initial.g1.3", "initial.g2.1", "initial.g2.2", "initial.g2.3"
+      #                              # "substream.g1.1", "substream.g1.2", "substream.g1.3", "substream.g2.1", "substream.g2.2", "substream.g2.3"
+      # )
       
+      assign(".Random.seed.clrng",  currentCreator, envir = .GlobalEnv)
       streamsR
       
     }
