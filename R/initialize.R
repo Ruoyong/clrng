74f21bc57a145538199de9e17e72baaf61273286
“Ruoyong <ruoyongxu2014@gmail.com>
2024-05-24 13:11:40 -0400

    edits on darjeeling

--- R/initialize.R
+++ R/initialize.R
@@ -1,29 +1,43 @@
 ###################################
 ### clrng global options  ###
 ###################################
+# .onLoad <- function(libname, pkgname) {
+#   # Set global options with default values
+#   options(clrng.Nglobal = c(16, 8), clrng.type = "double")
+#   
+#   if (detectGPUs()) {
+#     idx <- which(listContexts()$device_type == 'gpu')
+#     if (length(idx) > 0) {
+#       clrng.type <- c('float', 'double')[1 + gpuInfo(device_idx = 1L, context_idx = as.integer(idx))$double_support]
+#     }
+#   } else if (detectCPUs()) {
+#     idx <- which(listContexts()$device_type == 'cpu')
+#     if (length(idx) > 0) {
+#       clrng.type <- c('float', 'double')[1 + cpuInfo(device_idx = 1L, context_idx = as.integer(idx))$double_support]
+#     }
+#   } else {
+#     packageStartupMessage("No CPU nor GPU found on this platform. Setting clrng.type to 'float'.")
+#     options(clrng.type = 'float')
+#   }
+# }    
 
 .onLoad <- function(libname, pkgname) {
   # Set global options with default values
-    options(clrng.Nglobal = c(16, 8))
-    if (detectGPUs()) {
-      
-      idx <- which(listContexts()$device_type == 'gpu')
-      double_support = 1+gpuInfo(device_idx = 1L, context_idx = as.integer(idx))$double_support
-      
-    } else if (detectCPUs()) {
-      idx <- which(listContexts()$device_type == 'cpu')
-      double_support = 1+cpuInfo(device_idx = 1L, context_idx = as.integer(idx))$double_support
-      
-    } else{
-      message("No CPU nor GPU found on this platform.")
+  options(clrng.Nglobal = c(16, 8))
+  if (!(currentDevice()$device_type %in% c('cpu', 'gpu'))){
       options(clrng.type = 'float')
-      return()
-    }
-    clrng.type <- ifelse(double_support, "double", "float")
-    options(clrng.type = clrng.type)
+    }else{
+  options(clrng.type = c('float', 'double')[1 + gpuR::deviceHasDouble()])
+  }
 }
-      
-      
-    
+  
+  
+
+
+
+
+
+
+
 
 
