f47aef6b0c523d7565b8af3ec26269a09b00d436
“Ruoyong <ruoyongxu2014@gmail.com>
2024-05-26 08:44:29 +0000

    cran

--- R/initialize.R
+++ R/initialize.R
@@ -1,43 +1,63 @@
 ###################################
 ### clrng global options  ###
 ###################################
-# .onLoad <- function(libname, pkgname) {
-#   # Set global options with default values
-#   options(clrng.Nglobal = c(16, 8), clrng.type = "double")
-#   
-#   if (detectGPUs()) {
-#     idx <- which(listContexts()$device_type == 'gpu')
-#     if (length(idx) > 0) {
-#       clrng.type <- c('float', 'double')[1 + gpuInfo(device_idx = 1L, context_idx = as.integer(idx))$double_support]
-#     }
-#   } else if (detectCPUs()) {
-#     idx <- which(listContexts()$device_type == 'cpu')
-#     if (length(idx) > 0) {
-#       clrng.type <- c('float', 'double')[1 + cpuInfo(device_idx = 1L, context_idx = as.integer(idx))$double_support]
-#     }
-#   } else {
-#     packageStartupMessage("No CPU nor GPU found on this platform. Setting clrng.type to 'float'.")
-#     options(clrng.type = 'float')
-#   }
-# }    
-clrng_env <- new.env(parent = emptyenv())
 
 .onLoad <- function(libname, pkgname) {
   # Set global options with default values
   options(clrng.Nglobal = c(16, 8))
-  if (!(currentDevice()$device_type %in% c('cpu', 'gpu'))){
-      options(clrng.type = 'float')
-    }else{
-  options(clrng.type = c('float', 'double')[1 + gpuR::deviceHasDouble()])
+  
+  if (!(currentDevice()$device_type %in% c('cpu', 'gpu'))) {
+    options(clrng.type = 'float')
+  } else {
+    options(clrng.type = c('float', 'double')[1 + gpuR::deviceHasDouble()])
   }
+  
+  # Create package environment
+  clrng_env <<- new.env(parent = emptyenv())
+  ns <- asNamespace(pkgname)
+  assign("clrng_env", clrng_env, envir = ns)
+  
+  # the point of this file is to create empty functions 
+  # corresponding to names in the assertive package.
+  # if the assertive package becomes available again 
+  # these functions will be removed
+  # assertive.numbers 
+  
+  invisible(mapply(function(xx) assign(xx, function(...){}, pos=clrng_env), 
+                   xx = 
+                     c( paste0('assert_', 
+                               c('all_are_in_closed_range', 'assert_all_are_positive', 'all_are_in_range(')),
+                        
+                        #assertive.types 
+                        paste0('assert_is_', c('scalar', 'matrix', 'numeric', 'a_bool', 'a_number')),
+                        
+                        # assertive.properties
+                        'assert_is_of_length',
+                        # assertive.base
+                        c('get_name_in_parent', 'assert_engine', 'assert_all_are_true')
+                     )))
+  
 }
+
+
+
+
+
+# .onLoad <- function(libname, pkgname) {
+#   # Set global options with default values
+#   clrng_env <- new.env(parent = emptyenv())
+#   assign("clrng_env", clrng_env, envir = .GlobalEnv)
+#   
+#   options(clrng.Nglobal = c(16, 8))
+#   if (!(currentDevice()$device_type %in% c('cpu', 'gpu'))){
+#       options(clrng.type = 'float')
+#     }else{
+#   options(clrng.type = c('float', 'double')[1 + gpuR::deviceHasDouble()])
+#     }
+# 
+# }
   
   
-
-
-
-
-
 
 
 
