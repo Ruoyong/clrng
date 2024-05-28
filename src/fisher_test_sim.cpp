4dec01106a599311178cb2e32b576cb7ae945220
“Ruoyong <ruoyongxu2014@gmail.com>
2024-05-28 06:08:49 +0000

    copyrights in  description

--- src/fisher_test_sim.cpp
+++ src/fisher_test_sim.cpp
@@ -1,31 +1,14 @@
-/* 
- * Algorithm AS 159 Applied Statistics (1981), vol. 30, no. 1
- * original (C) Royal Statistical Society 1981
- * Generate random two-way table with given marginal totals.
- * Heavily pretty edited by Martin Maechler, Dec 2003
- * use double precision for integer multiplication (against overflow);
-
-  *  R : A Computer Language for Statistical Data Analysis
-  *  Copyright (C) 2001-5   The R Core Team.
-  *
-  *  This program is free software; you can redistribute it and/or modify
-  *  it under the terms of the GNU General Public License as published by
-  *  the Free Software Foundation; either version 2 of the License, or
-  *  (at your option) any later version.
-  *
-  *  This program is distributed in the hope that it will be useful,
-  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  *  GNU General Public License for more details.
-  *
-  *  You should have received a copy of the GNU General Public License
-  *  along with this program; if not, a copy is available at
-  *  http://www.r-project.org/Licenses/
-  
+/* Algorithm AS 159 Applied Statistics (1981), vol. 30, no. 1
+ original (C) Royal Statistical Society 1981
+ 
+ Generate random two-way table with given marginal totals.
+ 
+ Heavily pretty edited by Martin Maechler, Dec 2003
+ use double precision for integer multiplication (against overflow);
  */
 
 /*
- * the original files have been adapted
+ * the original files have been largely adapted
  */
 
 
